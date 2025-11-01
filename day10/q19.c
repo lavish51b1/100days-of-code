a520535ef220e64a4283a168703ab408b631b7c2
lavish51b1 <lavishgoyal0209@gmail.com>
2025-11-01 19:08:59 +0530

    Code:....

--- day10/q19.c
+++ day10/q19.c
@@ -1,31 +1,27 @@
-#include<stdio.h>
+#include <stdio.h>
 
-int main () {
+int main() {
+    int a, b, c;
 
-int a, b, c;
+    printf("Length of first side is: ");
+    scanf("%d", &a);
 
-printf("Length of first side is: ");
-scanf("%d", &a);
+    printf("Length of second side is: ");
+    scanf("%d", &b);
 
-printf("Length of second side is: ");
-scanf("%d", &b);
+    printf("Length of third side is: ");
+    scanf("%d", &c);
 
-printf("Length of third side is: ");
-scanf("%d", &c);
+    if (a == b && b == c) {
+        printf("It is an equilateral triangle\n");
+    } 
+    else if (a == b || a == c || b == c) {
+        printf("It is an isosceles triangle\n");
+    } 
+    else {
+        printf("It is a scalene triangle\n");
+    }
 
-if ( (a>b && a>c && b!=c) || (b>c && b>a && c!=a) || (c>a && c>b && a!=b)) {
-    printf("It is scalene triangle\n");
+    return 0;
 }
 
-else if ((a==b && a!=c) || (a==c && a!=b) || (b==c && b!=c)) {
-    printf("It is isoceles triangle\n");
-}
-
-else {
-    printf("It is equilatral triangle\n");
-}
-
-return 0;
-
-
-}
--- day14/q27.c
+++ day14/q27.c
@@ -2,19 +2,21 @@
 
 int main(){
 
-    int num,sum,i;
+    int n,num,sum,i;
       
       sum =0;
 
 
     printf("Enter a number: ");
-    scanf("%d",&num);
+    scanf("%d",&n);
+
+    num = 2*n-1;
 
     for(i=1; i<=num; i += 2) {
         
       sum = sum + i;
    }
-   printf("Sum of odd numbers upto %d is %d\n",num,sum);
+   printf("Sum of odd numbers upto %d is %d\n",n,sum);
    
     return 0;
 }  
